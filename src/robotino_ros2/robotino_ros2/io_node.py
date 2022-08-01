e2718b9093a0ba126662834aab62e4158cc239d0
Ziang <ziang.qiu@gmail.com>
2022-08-01 14:50:55 +0200

    add digital control

--- src/robotino_ros2/robotino_ros2/io_node.py
+++ src/robotino_ros2/robotino_ros2/io_node.py
@@ -1,8 +1,10 @@
 import rclpy
+import json
+import requests
 from rclpy.node import Node
 from robotino_ros2.config import ip_address
 from robotino_ros2_msg.srv import IOStatus
-import requests
+from robotino_ros2_msg.msg import OutputControl, OutputControlArray
 
 
 ip = "http://" + ip_address
@@ -13,6 +15,23 @@ class IONode(Node):
         super().__init__("io_node")
         self.controller_info_srv = self.create_service(
             IOStatus, 'io_status', callback=self.get_io_status)
+        self.digital_output_subscriber = self.subscriptions(
+            OutputControl, "digital_output_sub", self.digital_output_control, 10)
+        self.digital_output_array_subscriber = self.subscriptions(
+            OutputControlArray, "digital_output_array_sub", self.degital_output_array_control, 10)
+
+    def degital_output_control(self, msg):
+        request_json = {
+            "num": msg.NUM,
+            "val": msg.VAL
+        }
+        requests.post(ip+"/data/digitaloutput", data=json.dumps(request_json))
+
+    def degital_output_array_control(self, msg):
+        req_array = msg.array
+        if (len(req_array) == 8):
+            requests.post(ip+"/data/digitaloutputarray",
+                          data=json.dumps(req_array))
 
     def get_io_status(self, request, response):
         result = requests.get(ip + "/data/analoginputarray").json()
@@ -22,6 +41,7 @@ class IONode(Node):
         result = requests.get(ip + "/data/digitaloutputstatus").json()
         response.digital_output_array = result
         return response
+
 
 def main(args=None):
     rclpy.init(args=args)
