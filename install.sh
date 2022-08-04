21aea520a15d9849c6669248332eac912d66b22d
Ziang <ziang.qiu@gmail.com>
2022-08-04 16:43:34 +0200

    improve install workflow

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 sudo update
 sudo apt-get install libboost-all-dev ros-galactic-diagnostic-updater \
                         ros-galactic-laser-proc ros-galactic-urg-c \
-                        ros-galactic-camera-info-manager ros-galactic-image-transport
+                        ros-galactic-usb-cam
