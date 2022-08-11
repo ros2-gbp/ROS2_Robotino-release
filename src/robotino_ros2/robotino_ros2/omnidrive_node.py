8bb8f8a17669409c0d16a8298708fa58b6afcd7e
Ziang <ziang.qiu@gmail.com>
2022-08-11 17:38:09 +0200

    format file

--- src/robotino_ros2/robotino_ros2/omnidrive_node.py
+++ src/robotino_ros2/robotino_ros2/omnidrive_node.py
@@ -8,26 +8,31 @@ import json
 
 
 ip = "http://" + ip_address
-headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
+headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
 
 class OmnidriveNode(Node):
     def __init__(self):
         super().__init__("omnidrive_node")
-        self.omnidrive_subscription = self.create_subscription(Omnidrive,'omnidriver',self.omnidrive_callback,10)
-        self.teleop_drive_subscription = self.create_subscription(Twist,"/cmd_vel",self.teleop_drive_callback,10)
-        
-    def omnidrive_callback(self,msg):
-        request_json = [msg.vx,msg.vy,msg.omega]
-        requests.post(ip+"/data/omnidrive",data=json.dumps(request_json),headers=headers)
+        self.omnidrive_subscription = self.create_subscription(
+            Omnidrive, 'omnidriver', self.omnidrive_callback, 10)
+        self.teleop_drive_subscription = self.create_subscription(
+            Twist, "/cmd_vel", self.teleop_drive_callback, 10)
 
-    def teleop_drive_callback(self,msg):
+    def omnidrive_callback(self, msg):
+        request_json = [msg.vx, msg.vy, msg.omega]
+        requests.post(ip+"/data/omnidrive",
+                      data=json.dumps(request_json), headers=headers)
+
+    def teleop_drive_callback(self, msg):
         x = msg.linear.x * 0.3
         y = msg.linear.y * 0.3
         z = msg.angular.z * 0.3
-        request_json = [x,y,z]
+        request_json = [x, y, z]
         print(request_json)
-        requests.post(ip+"/data/omnidrive",data=json.dumps(request_json),headers=headers)
+        requests.post(ip+"/data/omnidrive",
+                      data=json.dumps(request_json), headers=headers)
+
 
 def main(args=None):
     rclpy.init(args=args)
