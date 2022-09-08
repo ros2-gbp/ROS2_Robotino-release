99ecb6cfadcaadc6445c72cbac1d6523dcbb51a2
Ziang <ziang.qiu@gmail.com>
2022-09-08 17:08:13 +0200

    add urdf

--- src/robotino_ros2/setup.py
+++ src/robotino_ros2/setup.py
@@ -14,6 +14,8 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'urdf/model'), glob('urdf/model/*.dae')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
