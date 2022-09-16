94d62370cdebbd2335237e76a85f6c5bb001ffe7
Ziang <ziang.qiu@gmail.com>
2022-09-16 11:40:09 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 sudo apt update
-sudo apt install libboost-all-dev ros-humble-diagnostic-updater \
-                        ros-humble-laser-proc ros-humble-urg-c \
-                        ros-humble-usb-cam
-sudo apt install ros-humble-urg-node-msgs
-sudo apt install ros-humble-robot-localization
-sudo apt install ros-humble-slam-toolbox
-sudo apt install ros-humble-joint-state-publisher-gui
-sudo apt install ros-humble-xacro
+sudo apt install libboost-all-dev ros-galactic-diagnostic-updater \
+                        ros-galactic-laser-proc ros-galactic-urg-c \
+                        ros-galactic-usb-cam
+sudo apt install ros-galactic-urg-node-msgs
+sudo apt install ros-galactic-robot-localization
+sudo apt install ros-galactic-slam-toolbox
+sudo apt install ros-galactic-joint-state-publisher-gui
+sudo apt install ros-galactic-xacro
