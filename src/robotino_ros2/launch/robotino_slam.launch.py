a0058202854f36d25a227bcc5db38453de427cdb
Ziang <ziang.qiu@gmail.com>
2022-09-16 11:35:08 +0200

    update slam and install

--- src/robotino_ros2/launch/robotino_slam.launch.py
+++ src/robotino_ros2/launch/robotino_slam.launch.py
@@ -3,43 +3,43 @@ from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 def generate_launch_description():
     robotino_ros2_dir = get_package_share_directory('robotino_ros2')    
     default_model_path = os.path.join(robotino_ros2_dir, 'urdf/robotino.urdf')
     default_rviz_config_path = os.path.join(robotino_ros2_dir, 'urdf/robotino.rviz')
 
-    robot_state_publisher_node = launch_ros.actions.Node(
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
     )
-    joint_state_publisher_node = launch_ros.actions.Node(
+    joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
+    joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
         condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
-    rviz_node = launch_ros.actions.Node(
+    rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
-
-    robot_localization_node = launch_ros.actions.Node(
-       package='robot_localization',
-       executable='ekf_node',
-       name='ekf_filter_node',
-       output='screen',
-       parameters=[os.path.join(robotino_ros2_dir, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}])
+    \
+    start_async_slam_toolbox_node = Node(
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen')
 
 
     return launch.LaunchDescription([
@@ -49,11 +49,38 @@ def generate_launch_description():
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                            description='Flag to enable use_sim_time'),
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        robot_localization_node,
         rviz_node,
+        Node(
+            package='robotino_ros2',
+            namespace='robotino',
+            executable='power_node',
+            name='power'
+        ),
+        Node(
+            package='robotino_ros2',
+            namespace='robotino',
+            executable='controller_node',
+            name='controller'
+        ),
+        Node(
+            package='robotino_ros2',
+            namespace='robotino',
+            executable='service_node',
+            name='service'
+        ),
+        Node(
+            package='robotino_ros2',
+            namespace='robotino',
+            executable='io_node',
+            name='io'
+        ),
+        Node(
+            package='robotino_ros2',
+            namespace='robotino',
+            executable='omnidrive_node',
+            name='omnidrive'
+        )
     ])
